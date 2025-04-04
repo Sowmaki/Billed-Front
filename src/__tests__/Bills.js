c9215b595f3621b2b741c37e7b6fdcaf88cfa3c0
sowmaki <solene.tassel@hotmail.com>
2025-04-04 12:39:24 +0200

    Couverture de tests sur Bills et NewBill

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -2,13 +2,16 @@
  * @jest-environment jsdom
  */
 
-import { screen, waitFor } from "@testing-library/dom";
+import { fireEvent, screen, waitFor } from "@testing-library/dom";
 import { localStorageMock } from "../__mocks__/localStorage.js";
-import { ROUTES_PATH } from "../constants/routes.js";
+import mockStore from "../__mocks__/store.js";
+import router from "../app/Router.js";
+import { ROUTES, ROUTES_PATH } from "../constants/routes.js";
+import Bills from "../containers/Bills.js";
 import { bills } from "../fixtures/bills.js";
 import BillsUI from "../views/BillsUI.js";
 
-import router from "../app/Router.js";
+jest.mock("../app/store", () => mockStore)
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -25,19 +28,124 @@ describe("Given I am connected as an employee", () => {
       window.onNavigate(ROUTES_PATH.Bills)
       await waitFor(() => screen.getByTestId('icon-window'))
       const windowIcon = screen.getByTestId('icon-window')
-      //to-do write expect expression
+      expect(windowIcon.classList.contains('active-icon')).toBeTruthy()
 
     })
     test("Then bills should be ordered from earliest to latest", () => {
-      // crée l'interface utilisateur avec les factures (bills) passées en paramètre.
       document.body.innerHTML = BillsUI({ data: bills })
-      // récupère tous les éléments contenant une date au format YYYY-MM-DD, YYYY/MM/DD ou YYYY.MM.DD
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
-      // Trie les dates du plus récent au plus ancien (ex : 2024-03-10 avant 2023-05-12).
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
-      // Crée copie triée de dates selon antiChrono. 
       const datesSorted = [...dates].sort(antiChrono)
       expect(dates).toEqual(datesSorted)
     })
+
+    describe('When I click on IconEye ', () => {
+      const icon = document.createElement('div')
+      icon.setAttribute('data-bill-url', 'https://fakeurl.com/bill.jpg');
+
+      $.fn.modal = jest.fn(); //mock de la fonction modal
+      const modal = document.createElement('div');
+      modal.setAttribute('id', 'modaleFile');
+      modal.innerHTML = '<div class="modal-body"></div>';
+      document.body.append(modal);
+
+      const billsInstance = new Bills({ document, onNavigate: jest.fn(), store: mockStore, localStorage: window.localStorage });
+      billsInstance.handleClickIconEye(icon);
+
+      const modalBody = document.querySelector('.modal-body');
+      expect(modalBody.innerHTML).toContain('https://fakeurl.com/bill.jpg');
+      expect($.fn.modal).toHaveBeenCalledWith('show')
+    });
+
+
+    describe('When I click on new bill button', () => {
+      test(('Then, I should be sent to newBill page'), () => {
+        const daBills = [{
+          "id": "47qAXb6fIm2zOKkLzMro",
+          "vat": "80",
+          "fileUrl": "https://test.storage.tld/v0/b/billable-677b6.a…f-1.jpg?alt=media&token=c1640e12-a24b-4b11-ae52-529112e9602a",
+          "status": "pending",
+          "type": "Hôtel et logement",
+          "commentary": "séminaire billed",
+          "name": "encore",
+          "fileName": "preview-facture-free-201801-pdf-1.jpg",
+          "date": "2004-04-04",
+          "amount": 400,
+          "commentAdmin": "ok",
+          "email": "a@a",
+          "pct": 20,
+        }]
+
+        const onNavigate = (pathname) => {
+          document.body.innerHTML = ROUTES({ pathname })
+        }
+
+        Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+        window.localStorage.setItem('user', JSON.stringify({
+          type: 'Employee'
+        }))
+
+        const bills = new Bills({ document, onNavigate, localStorage })
+        const handleClick = jest.fn(bills.handleClickNewBill.bind(bills))
+        document.body.innerHTML = BillsUI({ data: daBills })
+
+        const btnNewBill = screen.getByTestId('btn-new-bill')
+        btnNewBill.addEventListener("click", handleClick);
+
+        fireEvent.click(btnNewBill);
+        expect(handleClick).toHaveBeenCalled();
+      })
+    })
+
+    describe("When API data is corrupted", () => {
+      beforeEach(() => {
+        jest.spyOn(mockStore, "bills");
+        Object.defineProperty(window, "localStorage", { value: localStorageMock });
+        window.localStorage.setItem("user", JSON.stringify({ type: "Employee", email: "a@a" }));
+        document.body.innerHTML = `<div id="root"></div>`;
+        router();
+      });
+
+      test("date format is not valid, log the error and return unformatted date", async () => {
+        const consoleErrorSpy = jest.spyOn(console, "log").mockImplementation(() => { });
+
+        mockStore.bills.mockImplementationOnce(() => {
+          return {
+            list: () => Promise.resolve([{
+              vat: 0,
+              status: "",
+              type: "",
+              commentary: "",
+              name: "",
+              fileName: "",
+              filePath: "",
+              date: "fakeDate",
+              amount: 0,
+              commentAdmin: "",
+              pct: 0,
+              email: "",
+              key: "bklbhfdr",
+            }]),
+          };
+        });
+
+        window.onNavigate(ROUTES_PATH.Bills);
+        await new Promise(process.nextTick); // Attendre la fin de l'exécution des promesses
+
+        // S'assurer que la date est bien affichée non-formatée
+        const message = await screen.getByText(/fakeDate/);
+        expect(message).toBeTruthy();
+
+        // S'assurer que la console log bien l'erreur
+        expect(consoleErrorSpy).toHaveBeenCalled();
+        expect(consoleErrorSpy.mock.calls[0][1]).toBe("for", expect.objectContaining({ date: "fakeDate" }));
+
+      });
+
+    });
+
+
   })
-})
+
+});
+
--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -2,17 +2,79 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
-import NewBillUI from "../views/NewBillUI.js"
+import { fireEvent, screen, waitFor } from "@testing-library/dom"
+import userEvent from "@testing-library/user-event"
+import { localStorageMock } from "../__mocks__/localStorage.js"
+import mockStore from "../__mocks__/store.js"
+import { ROUTES_PATH } from "../constants/routes.js"
 import NewBill from "../containers/NewBill.js"
+import NewBillUI from "../views/NewBillUI.js"
 
+jest.mock("../app/Store.js", () => mockStore); // Mock du store
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
+
+    beforeEach(() => {
+      jest.clearAllMocks()
+      Object.defineProperty(window, "localStorage", { value: localStorageMock })
+      window.localStorage.setItem("user", JSON.stringify({ type: "Employee", email: "a@a" }))
+      document.body.innerHTML = NewBillUI()
+
     })
+
+    //Upload d'un fichier valide
+    test("Then I can upload a valid file", async () => {
+      const onNavigate = jest.fn()
+      const newBill = new NewBill({ document, onNavigate, store: mockStore, localStorage: window.localStorage })
+
+      const fileInput = screen.getByTestId("file")
+      const file = new File(["img"], "facture.jpg", { type: "image/jpg" })
+
+      userEvent.upload(fileInput, file)
+
+      await waitFor(() => expect(newBill.fileUrl).toBeDefined())
+      expect(newBill.fileName).toBe("facture.jpg")
+    })
+
+    test("Throw Error if file format is not valid", () => {
+      window.alert = jest.fn()
+      const newBill = new NewBill({ document, onNavigate: jest.fn(), store: mockStore, localStorage: window.localStorage })
+
+      const fileInput = screen.getByTestId("file")
+      const badFile = new File(["test"], "document.pdf", { type: "application/pdf" })
+
+      userEvent.upload(fileInput, badFile)
+
+      expect(window.alert).toHaveBeenCalledWith("Seuls les formats jpg, jpeg et png sont pris en compte.")
+      expect(fileInput.value).toBe("")
+    })
+
+    test("Submit form with valid data", async () => {
+      // document.body.innerHTML = NewBillUI()
+      const onNavigate = jest.fn()
+      const newBill = new NewBill({ document, onNavigate, store: mockStore, localStorage: window.localStorage })
+
+      // Fake une image uploadée
+      newBill.fileUrl = "https://localhost:3456/images/test.jpg"
+      newBill.fileName = "test.jpg"
+      newBill.billId = "1234"
+
+      fireEvent.change(screen.getByTestId("expense-type"), { target: { value: "Transports" } })
+      fireEvent.change(screen.getByTestId("expense-name"), { target: { value: "Vol Paris" } })
+      fireEvent.change(screen.getByTestId("datepicker"), { target: { value: "2023-04-01" } })
+      fireEvent.change(screen.getByTestId("amount"), { target: { value: "150" } })
+      fireEvent.change(screen.getByTestId("vat"), { target: { value: "20" } })
+      fireEvent.change(screen.getByTestId("pct"), { target: { value: "20" } })
+      fireEvent.change(screen.getByTestId("commentary"), { target: { value: "Un commentaire" } })
+
+      fireEvent.submit(screen.getByTestId("form-new-bill"))
+
+      await waitFor(() => {
+        expect(onNavigate).toHaveBeenCalledWith(ROUTES_PATH.Bills)
+      })
+    })
+
+
   })
 })
